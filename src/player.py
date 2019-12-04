8daf62fbb76670acbc0c5a78158ab8e60c04ae51
Chris York <christopherayork1992@gmail.com>
2019-12-04 17:58:16 -0500

    not working

--- src/player.py
+++ src/player.py
@@ -1,72 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-from room import Room
+# from room import Room
+from mob import Mob
 
 
-class Player:
-    def __init__(self, name, room):
-        self.name = name
-        self.room = room
-        self.items = []
+class Player(Mob):
+    def __init__(self, name, desc, room):
+        Mob.__init__(self, name, desc, room)
 
     def inventory(self):
         print('### Your inventory ###')
         for i in self.items:
             print(i)
-
-    def move(self, d):
-        if not isinstance(d, str):
-            return False
-        elif d == 'n' or d == 'north':
-            if isinstance(self.room.n_to, Room):
-                self.room = self.room.n_to
-                return True
-            else:
-                print('You can\'t move that way.')
-        elif d == 'e' or d == 'east':
-            if isinstance(self.room.e_to, Room):
-                self.room = self.room.e_to
-                return True
-            else:
-                print('You can\'t move that way.')
-        elif d == 's' or d == 'south':
-            if isinstance(self.room.s_to, Room):
-                self.room = self.room.s_to
-                return True
-            else:
-                print('You can\'t move that way.')
-        elif d == 'w' or d == 'west':
-            if isinstance(self.room.w_to, Room):
-                self.room = self.room.w_to
-                return True
-            else:
-                print('You can\'t move that way.')
-        return False
-
-    def grab(self, query):
-        found = False
-        for item in self.room.items:
-            if query == item.name:
-                found = True
-                grabbed = item.on_take()
-                if not grabbed:
-                    return False
-                self.items.append(item)
-                self.room.remove_item(item)
-                break
-        if not found:
-            print('Invalid item')
-
-    def drop(self, query):
-        found = False
-        for item in self.items:
-            if query == item.name:
-                found = True
-                dropped = item.on_drop()
-                if not dropped:
-                    return False
-                self.items.remove(item)
-                self.room.add_item(item)
-                break
-        if not found:
-            print('Invalid item')
--- src/room.py
+++ src/room.py
@@ -1,11 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from mob import Mob
 
 
-class Room:
+class Room(Mob):
     def __init__(self, name, desc):
         self.name = name
         self.desc = desc
+        self.mobs = []
         self.items = []
         self.n_to = False
         self.e_to = False
@@ -28,3 +30,22 @@ class Room:
 
     def remove_item(self, item):
         self.items.remove(item)
+
+    def enter(self, mob):
+        if not isinstance(mob, Mob):
+            return False
+        self.entered(mob)
+
+    def entered(self, mob):
+        allowed = mob.move_to(self)
+        if not allowed:
+            return False
+        self.mobs.append(mob)
+        return True
+
+    def exit(self, mob):
+        self.exited(mob)
+        return True
+
+    def exited(self, mob):
+        return True
